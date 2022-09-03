19b1cd9d45a6fbd656c7749f68357c4d8917befd
mansourov <mansurov.jr@mail.ru>
2022-09-04 01:05:31 +0500

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,53 @@
+import airbnb from "./img/airbnb.png";
+import bill from "./img/bill.png";
+import binance from "./img/binance.png";
+import card from "./img/card.png";
+import coinbase from "./img/coinbase.png";
+import dropbox from "./img/dropbox.png";
+import logo from "./svg/logo.svg";
+import quotes from "./svg/quotes.svg";
+import robot from "./img/robot.png";
+import send from "./svg/Send.svg";
+import shield from "./svg/Shield.svg";
+import star from "./svg/Star.svg";
+import menu from "./svg/menu.svg";
+import close from "./svg/close.svg";
+import google from "./svg/google.svg";
+import apple from "./svg/apple.svg";
+import arrowUp from "./svg/arrow-up.svg";
+import discount from "./svg/Discount.svg";
+import facebook from "./svg/facebook.svg";
+import instagram from "./svg/instagram.svg";
+import linkedin from "./svg/linkedin.svg";
+import twitter from "./svg/twitter.svg";
+import people01 from "./img/people01.png";
+import people02 from "./img/people02.png";
+import people03 from "./img/people03.png";
+
+export {
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { logo, menu, close } from "../assets";
+import { navLinks } from "../constants/index";
+
+const Navbar = () => {
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="w-full py-6 flex justify-between items-center navbar">
+      <img className="w-[124px] h-[32px]" src={logo} alt="HooBank" />
+
+      <ul className="sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end itmes-center">
+        <img
+          src={toggle ? close : menu}
+          alt="Menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
+
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className=" justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
